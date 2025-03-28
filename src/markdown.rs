3f9b697ef9f5ef47162189aa659c51f3cc6d1ddf
Merrick Young <merrick.young@gmail.com>
2025-03-28 14:17:23 -0500

    Added newlines for better formatting

--- src/markdown.rs
+++ src/markdown.rs
@@ -6,21 +6,27 @@ impl MarkupEngine for MarkdownEngine {
     fn header(&self, level: u8, text: &str) -> String {
         format!("{} {}", "#".repeat(level as usize), text)
     }
+
     fn paragraph(&self, text: &str) -> String {
         format!("{}\n", text)
     }
+
     fn italic(&self, text: &str) -> String {
         format!("*{}*", text)
     }
+
     fn bold(&self, text: &str) -> String {
         format!("**{}**", text)
     }
+
     fn link(&self, text: &str, url: &str) -> String {
         format!("[{}]({})", text, url)
     }
+
     fn linebreak(&self) -> String {
         format!("\n")
     }
+
     fn line_segment(&self, text: &str) -> String {
         text.to_string()
     }
